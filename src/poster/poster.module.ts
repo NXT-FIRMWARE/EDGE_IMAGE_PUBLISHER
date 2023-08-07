7250c3d029166e871d651a63d97473ee83a66e84
Venus00 <oussama@nextronic.io>
2023-08-07 15:48:56 +0100

    fix

--- src/poster/poster.module.ts
+++ src/poster/poster.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PosterService } from './poster.service';
+import { CameraModule } from 'src/camera/camera.module';
 
 @Module({
+  imports: [CameraModule],
   providers: [PosterService],
 })
 export class PosterModule {}
