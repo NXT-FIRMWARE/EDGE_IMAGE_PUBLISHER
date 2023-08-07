5a366739b16cebf38d3aae2bb701530c5bd3a4c4
Venus00 <oussama@nextronic.io>
2023-08-07 12:01:38 +0100

    fix

--- src/camera/camera.service.ts
+++ src/camera/camera.service.ts
@@ -14,7 +14,7 @@ export class CameraService {
   private recorder: Recorder[] = [];
   public date = new Date();
   private logger = new Logger('CAMERA_SERVICE');
-
+  private path = '';
   constructor() {
     // this.initRecorder();
   }
@@ -38,21 +38,26 @@ export class CameraService {
     for (let i = 0; i < this.recorder.length; i++) {
       await this.recorder[i].recorder.captureImage((filename) => {
         this.logger.log('image saved to ', this.recorder[i].recorder.folder);
-        console.log(filename);
-        //post request
-        this.PostRealTime();
+        console.log('image saved sucefully');
+        if (data[i].uuid !== '') {
+          this.PosteCreateId(filename, data[i].cameraName, i);
+        }
+        this.PostImage(filename, data[i].cameraName);
       });
     }
   }
 
-  async PostRealTime() {
-    const filename = 'C:/Users/jbray/Desktop/hello.png';
-    const data = new FormData();
+  async PosteCreateId(
+    filename: string,
+    cameraName: string,
+    indexCamera: number,
+  ) {
+    // const filename = 'C:/Users/jbray/Desktop/hello.png';
     const image = fs.createReadStream(filename);
     data.append('image', image);
     data.append('time', new Date().toLocaleString());
     data.append('location', process.env.LOCATION);
-    data.append('cameraName', 'DOME1');
+    data.append('cameraName', cameraName);
     await axios
       .post('http://192.168.1.29:3001/server', data, {
         headers: {
@@ -64,6 +69,43 @@ export class CameraService {
       .then((response) => {
         //handle success
         console.log(response.data);
+        //load id camera
+        data[indexCamera].uuid = response.data.id;
+        fs.writeFileSync('data.json', JSON.stringify(data));
+        //delete image
+        this.deleteImage(`${process.env.IMAGE_PATH}/${filename}`);
+      })
+      .catch((error) => {
+        //handle error
+        console.log(`${error}`);
+      });
+  }
+
+  async deleteImage(path: string) {
+    return fs.unlinkSync(path);
+  }
+  async PostImage(filename: string, cameraName: string) {
+    // const filename = 'C:/Users/jbray/Desktop/hello.png';
+    const data = new FormData();
+    const image = fs.createReadStream(filename);
+    data.append('image', image);
+    data.append('time', new Date().toLocaleString());
+    data.append('location', process.env.LOCATION);
+    data.append('cameraName', cameraName);
+    await axios
+      .post('http://192.168.1.29:3001/server', data, {
+        headers: {
+          accept: 'application/json',
+          'Accept-Language': 'en-US,en;q=0.8',
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        //handle success
+        console.log(response.data);
+        //delete image
+        this.deleteImage(`${process.env.IMAGE_PATH}/${filename}`);
+        //To  DO remove image from folder
       })
       .catch((error) => {
         //handle error
--- src/poster/poster.service.ts
+++ src/poster/poster.service.ts
@@ -10,7 +10,6 @@ export class PosterService {
   }
 
   postImages() {
-    
 
   }
 }
