f028bf1077be71968ff33a2fd6e3b8a6f8ea680b
Venus00 <oussama@nextronic.io>
2023-08-07 16:14:59 +0100

    fix

--- src/poster/poster.service.ts
+++ src/poster/poster.service.ts
@@ -30,7 +30,7 @@ export class PosterService implements OnModuleInit {
   @Cron('* * * * *')
   async filesUploader() {
     this.cameraConfig = this.cameraService.getCamraConfig();
-    this.logger.log('[d]  upload files to ftp ...');
+    this.logger.log('[d]  upload files to server ...');
     try {
       const cameraPath = this.getDirectories(this.path);
       for (let camerIndex = 0; camerIndex < cameraPath.length; camerIndex++) {
