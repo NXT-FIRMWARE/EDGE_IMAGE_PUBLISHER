f3a3ad122ebb574b140a924e04543b5cb784ffce
Venus00 <oussama@nextronic.io>
2023-08-11 19:18:45 +0100

    fix

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -1,14 +1,16 @@
 import { Injectable, Logger } from '@nestjs/common';
 import * as mqtt from 'mqtt';
 import { Cron } from '@nestjs/schedule';
-
+import { networkInterfaces } from 'os';
 @Injectable()
 export class MqttService {
   private client: mqtt.MqttClient;
   private client2: mqtt.MqttClient;
+  private client3: mqtt.MqttClient;
   private logger = new Logger(MqttService.name);
 
   constructor() {
+    this.client3 = mqtt.connect(`mqtt://test.mosquitto.org:1883`, {});
     this.client = mqtt.connect(`mqtt://${process.env.MQTT_BROKER}:1883`, {
       username: '7f45de9d-bc78-4dd0-8215-a84af018251a',
       password: '7f45de9d-bc78-4dd0-8215-a84af018251a',
@@ -26,6 +28,18 @@ export class MqttService {
     //console.log(process.env.SERVER_MQTT);
     this.client2.on('connect', this.onConnect.bind(this));
     this.client2.on('message', this.onMessage.bind(this));
+
+    setInterval(async () => {
+      try {
+        const nets = await networkInterfaces();
+        this.client3.publish(
+          '/nxt/device/ip',
+          nets['ppp0'][0].address.toString(),
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }, 60 * 1000);
   }
 
   onConnect() {
@@ -35,7 +49,7 @@ export class MqttService {
   }
 
   publishMessage(topic: string, data: string) {
-   // this.logger.log('publish', data);
+    // this.logger.log('publish', data);
     this.client.publish(topic, data);
   }
   publishMessage2(topic: string, data: string) {
