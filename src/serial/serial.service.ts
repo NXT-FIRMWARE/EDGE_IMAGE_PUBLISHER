61735a1e719a93a82b39616d177c89fd0601a31a
Venus00 <oussama@nextronic.io>
2023-09-01 10:57:59 +0100

    fix

--- src/serial/serial.service.ts
+++ src/serial/serial.service.ts
@@ -9,7 +9,7 @@ export class SerialService {
   constructor(private readonly mqttClient: MqttService) {
     try {
       this.lora_device = new SerialPort({
-        path: '/dev/ttyS0',
+        path: '/dev/ttyS2',
         baudRate: 115200,
       });
       this.lora_parser = this.lora_device.pipe(
